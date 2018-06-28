7e31b802ec44374ef38369dc0c59019d4ad651db
Fergus <fergus.jingtai@gmail.com>
2018-06-28 11:58:31 +0800

    add frontend file

---
+++ frontend/src/containers/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Counter from '../../components/Counter/Counter';
+
+class Home extends Component {
+  render() {
+    return (
+      <div id="app">
+        <Counter />
+      </div>
+    );
+  }
+}
+
+export default Home;
