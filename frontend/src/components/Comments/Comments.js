d4f86a3efcff1fa0b4cfede16d3780b84f52aedf
Fergus <fergus.jingtai@gmail.com>
2018-06-29 18:03:14 +0800

    update layout

--- frontend/src/components/Comments/Comments.js
+++ frontend/src/components/Comments/Comments.js
@@ -6,16 +6,14 @@ import './Comments.scss';
 class Comments extends Component {
   render() {
     return (
-      <div className="container mt-3">
-        <h1>Comments</h1>
-        <div className="form-group mt-3">
-          <form>
+      <div className="row mt-3">
+        <h1 className="w-100">Comments</h1>
+        <form>
+          <div className="form-group mt-3">
             <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" />
             <button className="btn btn-primary mt-2" type="submit">Submit</button>
-          </form>
-        </div>
-        <Media />
-        <Media />
+          </div>
+        </form>
       </div>
     );
   }
